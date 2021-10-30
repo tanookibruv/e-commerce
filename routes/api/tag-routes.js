4f40f11d509b450bd35a0c2502004c65fb7b5fe6
tanookibruv <erendon1836@gmail.com>
2021-10-30 09:23:47 -0700

    all routes working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -30,16 +30,54 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newtagData = await Tag.create({
+      tag_name: req.body.tag_name
+    });
+    res.status(200).json(newtagData);
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update(
+      {
+        tag_name: req.body.tag_name
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(updateTag)
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteData = await Tag.destroy(
+      {
+        where: {
+          id: req.params.id,
+        },
+    });
+    if (!deleteData) {
+      res.status(404).json({ message: 'This id does not belong to any Tag.'})
+      return;
+    }
+    res.status(200).json(deleteData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
